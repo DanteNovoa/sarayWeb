a4879614946db28a07d9c9cb5e481a69c0319d20
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-01-27 22:51:27 -0600

    nodemon installed mongoose required in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 // Import expressjs
 const express = require('express');
+const mongoose = require('./database');
 
 // Create an instance from the object express
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(require('./routes/index'));
 
 // Initialize the server in port 3000
+// nodemom module is installed to restart the server authomaticaly with each change
 app.listen(3000);
 // Show a message in the console
 console.log(`Listen on port 3000`);
