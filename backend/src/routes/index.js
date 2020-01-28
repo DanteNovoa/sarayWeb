b32c248b6ff576d667ffe8f5bf9afe72648ca22d
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-01-27 23:04:06 -0600

    api path deleted and new path with post added

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,8 +5,12 @@ const router = Router();
 const User = require('../models/User');
 
 //Create a new route with the method GET
-router.get('/api', (req, res) => {
+router.get('/', (req, res) => {
     res.send(`Hello World!`);
+});
+
+router.post('/signup', (req, res) =>{
+    res.send(`New user`);
 });
 
 // Export the object that contains the routes
